a8f7c6f85ecd265470b16874816f69350a62ce41
Satyam jha <73050094+75atyam@users.noreply.github.com>
2024-01-18 10:06:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSAUJOiIwEvu-w9WKnC84incHQmIj7v2E",
+    authDomain: "theem-doc.firebaseapp.com",
+    projectId: "theem-doc",
+    storageBucket: "theem-doc.appspot.com",
+    messagingSenderId: "407111670911",
+    appId: "1:407111670911:web:3f7857bbd5262aa586334a",
+    measurementId: "G-SKQMMJQDXD"
+  };
+export {firebaseConfig};
+
+
